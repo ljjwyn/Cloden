8f83bc20b1a8f66fc8eb920e922e10f71e263305
lijj-m <15964969802@163.com>
2021-08-02 21:05:50 +0800

    修改

--- src/main/java/com/example/gloden/interceptor/DefaultInt.java
+++ src/main/java/com/example/gloden/interceptor/DefaultInt.java
@@ -1,5 +1,25 @@
 package com.example.gloden.interceptor;
 
+import com.example.gloden.annotation.Default;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.lang.reflect.Method;
+
+@Aspect
 public class DefaultInt {
-    
+    @Pointcut("@annotation(com.example.gloden.annotation.Default)")
+    public void annotationPointcut() {}
+
+    @Before("annotationPointcut()")
+    public void beforePointcut(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        Default annotation = method.getAnnotation(Default.class);
+
+    }
+
 }
